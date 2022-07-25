b57737aa3220f600214309c48671ef76b72c3101
郝程宇 <haochengyu@yidian-inc.com>
2022-07-25 16:59:27 +0800

    fix(menu): delete menu back login

--- src/layout/User.vue
+++ src/layout/User.vue
@@ -4,20 +4,20 @@
       <a-avatar :src="avatar || STATE.DEFAULT_AVATAR" :alt="`${name}'s avatar`"></a-avatar>
       <span class="user-name">{{ name || '管理员' }}</span>
     </div>
-    <template #overlay>
+    <!-- <template #overlay>
       <a-menu>
         <a-menu-item key="1">
           <RollbackOutlined />
           <span><a class="link" href="/">返回登录</a></span>
         </a-menu-item>
       </a-menu>
-    </template>
+    </template> -->
   </a-dropdown>
 </template>
 
 <script lang="ts" setup>
 import { reactive } from 'vue';
-import { RollbackOutlined } from '@ant-design/icons-vue';
+// import { RollbackOutlined } from '@ant-design/icons-vue';
 import appConfig from '@/config/app.config';
 const props = defineProps({
   name: {
